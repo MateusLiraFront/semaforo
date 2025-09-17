0b4af762eb95afbc326d8f8fef74ddfa781e9785
MateusLiraFront <mlgtifront@gmail.com>
2025-09-17 15:02:58 -0300

    logica

---
+++ script.js
@@ -0,0 +1,11 @@
+function trocarCorDoSemaforoParaNovaCorDiferenteDaAnterior(){
+    let cor = ['verdeLimao', 'amareloDourado', 'vermelhoCarmesin'];
+
+    while(4=!0){
+        setTimeout(function(){
+                cor[0]
+        
+            }, (corAtual == 'Amarelo') ? 1000 : 8000); 
+    }
+     
+}
