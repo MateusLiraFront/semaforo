769da8388c966d50bbc65fb2193e28c4ba369ce0
Erick Lima <erick.monteiroeflm@gmail.com>
2025-09-17 16:39:32 -0300

    script de mudar de cor

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-function trocarCorDoSemaforoParaNovaCorDiferenteDaAnterior(){
-    let cor = ['vermelho', 'amarelo', 'verde'];
-    
-        setTimeout(function(){
-            for(i=0; i<3; i++){
-                let corAtual = document.getElementById(cor[i]);
-                if(corAtual){
-                    corAtual.style.boxShadow = "var(--brilho)"+cor[i];
-                }else{
-                    corAtual.style.boxShadow = "var(--apagado)"+cor[i];
-                }
-            }
-            
-        }, (corAtual == 'Amarelo') ? 1000 : 8000); 
+let state = Boolean
 
+function pararSemaforo () {
+    state = false
+}
+
+function iniciarSemaforo () {
+    state = true
+    return trocarCorDoSemaforoParaNovaCorDiferenteDaAnterior(0)
+}
+
+function trocarCorDoSemaforoParaNovaCorDiferenteDaAnterior(index){
+    let cor = ['green','yellow', 'red' ];
+
+    setTimeout(function(){
+        if (index == 0)
+            {
+                apagarSemaforo()
+                acenderSemaforoAtual(cor[index])
+            }
+        else if (index == 1)
+            {
+                apagarSemaforo()
+                acenderSemaforoAtual(cor[index])
+            } 
+        else
+            {
+                apagarSemaforo(cor[index])
+                acenderSemaforoAtual(cor[index])
+            }
+        }, (index == 1) ? 1000 : 8000);
+    if (!state) {
+        return 
+    } else if (index == 2){
+        trocarCorDoSemaforoParaNovaCorDiferenteDaAnterior(index = 0)
+    } else {
+        trocarCorDoSemaforoParaNovaCorDiferenteDaAnterior(index++)
+    }
+}
+
+function apagarSemaforo(index) {
+    let corAtual = document.getElementById(cor[index])
+    corAtual.style.boxShadow = "var(--apagado)"+cor[index]
+}
+
+function acenderSemaforoAtual(index) {
+    let corAtual = document.getElementById(cor[index])
+    corAtual.style.boxShadow = "var(--brilho)"+cor[index]
 }
